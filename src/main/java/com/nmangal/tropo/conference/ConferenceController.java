e492f7a8c6434e5f1182d4391b42e58fbaef63a2
neerajmangal <neerajmangal@users.noreply.github.com>
2017-04-05 11:53:31 +0530

    Update ConferenceController.java

--- src/main/java/com/nmangal/tropo/conference/ConferenceController.java
+++ src/main/java/com/nmangal/tropo/conference/ConferenceController.java
@@ -1,39 +1,29 @@
 package com.nmangal.tropo.conference;
 
-import java.util.Map;
+import static com.voxeo.tropo.Key.ID;
+import static com.voxeo.tropo.Key.NAME;
+import static com.voxeo.tropo.Key.EVENT;
 
-import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.voxeo.tropo.Tropo;
+import com.voxeo.tropo.actions.ConferenceAction;
+import com.voxeo.tropo.actions.Do;
 
 @RestController
 public class ConferenceController {
 
 	@RequestMapping(value = "/conference/{conferenceID}", consumes = { "application/json" }, produces = {
 			"application/json" }, method = RequestMethod.POST)
-	public @ResponseBody UserStory getConference(@PathVariable(value = "conferenceID") final String storyNumber) {
-		UserStory userStory = new UserStory(storyNumber);
-		return userStory;
+	public String makeConference(@PathVariable(value = "conferenceID") final String conferenceID) {
+		// Put the call in conference
+		Tropo tropo = new Tropo();
+		ConferenceAction confAction = tropo.conference(NAME("NeerajMangal"), ID(conferenceID));
+		confAction.and(Do.on(EVENT("join")).and(Do.say("Welcome to Conference")));
+		return tropo.text();
 	}
 
-	@RequestMapping(value = "/conference/{conferenceID}", consumes = { "application/json" }, produces = {
-	"application/json" }, method = RequestMethod.POST)
-	public @ResponseBody ResponseEntity<UserStory> makeConference(@PathVariable(value = "conferenceID") final String conferenceID,
-			@Valid @RequestBody final Map<String, String> request) {
-		RallyUpdateUserStory updateStoryState = new RallyUpdateUserStory();
-		
-		if (updateStoryState.RallyUpdateStoryState(storyNumber, request.get("state"))){
-			UserStory userStory = new UserStory(storyNumber);
-			return new ResponseEntity<UserStory>(userStory, HttpStatus.OK);
-		}
-		return new ResponseEntity<UserStory>(HttpStatus.INTERNAL_SERVER_ERROR);
-		
-	}
 }
