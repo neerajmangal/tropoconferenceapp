c980e8af92ce323f3304be4001f8294862f22cfb
neerajmangal <neerajmangal@users.noreply.github.com>
2017-04-05 10:56:59 +0530

    Create Application.java

---
+++ src/main/java/com/nmangal/tropo/conference/Application.java
@@ -0,0 +1,15 @@
+package com.nmangal.tropo.conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+
+@SpringBootApplication
+@EnableSpringConfigured
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
