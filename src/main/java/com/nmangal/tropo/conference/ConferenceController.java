77942f477017545ee888d55592f5c4c83e39e290
neerajmangal <neerajmangal@users.noreply.github.com>
2017-04-06 06:19:46 +0530

    Update ConferenceController.java

--- src/main/java/com/nmangal/tropo/conference/ConferenceController.java
+++ src/main/java/com/nmangal/tropo/conference/ConferenceController.java
@@ -4,7 +4,11 @@ import static com.voxeo.tropo.Key.ID;
 import static com.voxeo.tropo.Key.NAME;
 import static com.voxeo.tropo.Key.EVENT;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +20,13 @@ import com.voxeo.tropo.actions.Do;
 @RestController
 public class ConferenceController {
 
-	@RequestMapping(value = "/conference/{conferenceID}", consumes = { "application/json" }, produces = {
+	@RequestMapping(value = "/conference/", consumes = { "application/json" }, produces = {
 			"application/json" }, method = RequestMethod.POST)
-	public String makeConference(@PathVariable(value = "conferenceID") final String conferenceID) {
+	public String makeConference(@valid @RequestBody final Map<String, String> request) {
 		// Put the call in conference
+		String conferenceID = request.get("conferenceID");
+		String conferenceName = request.get("name");
+		
 		Tropo tropo = new Tropo();
 		ConferenceAction confAction = tropo.conference(NAME("NeerajMangal"), ID(conferenceID));
 		confAction.and(Do.on(EVENT("join")).and(Do.say("Welcome to Conference")));
