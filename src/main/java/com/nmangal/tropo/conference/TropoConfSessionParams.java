6173586c852ff92907f61ddd8eb46a4aa181a560
neerajmangal <neerajmangal@users.noreply.github.com>
2017-04-06 12:20:42 +0530

    Create TropoConfSessionParams.java

---
+++ src/main/java/com/nmangal/tropo/conference/TropoConfSessionParams.java
@@ -0,0 +1,34 @@
+package com.nmangal.tropo.conference;
+
+public class TropoConfSessionParams {
+	
+	private String name;
+	private String conferenceID;
+	/**
+	 * @return the conferenceID
+	 */
+	public String getConferenceID() {
+		return conferenceID;
+	}
+	/**
+	 * @param conferenceID the conferenceID to set
+	 */
+	public void setConferenceID(String conferenceID) {
+		this.conferenceID = conferenceID;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
