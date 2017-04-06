c117c7b48c8d5c4d82666537b0dcbe7f6247e6b2
neerajmangal <neerajmangal@users.noreply.github.com>
2017-04-06 12:20:18 +0530

    Create TropoConferenceSession.java

---
+++ src/main/java/com/nmangal/tropo/conference/TropoConferenceSession.java
@@ -0,0 +1,23 @@
+package com.nmangal.tropo.conference;
+
+
+public class TropoConferenceSession {
+    
+	private ConferenceSession session;
+
+	/**
+	 * @return the session
+	 */
+	public ConferenceSession getSession() {
+		return session;
+	}
+
+	/**
+	 * @param session the session to set
+	 */
+	public void setSession(ConferenceSession session) {
+		this.session = session;
+	}
+	
+	
+}
